949ae1303ea7af30ee99906cd34a4753ada584f8
Victor Guerreiro <105592007+ViictorWebTech@users.noreply.github.com>
2023-03-18 10:06:26 -0300

    adicionado um botão para ativar o modo noturno, com js

---
+++ switchtema.js
@@ -0,0 +1,32 @@
+const tema = {
+    light: {
+      background: 'white',
+      text: 'black',
+    },
+    dark: {
+      background: 'black',
+      text: 'white',
+    }
+};
+
+
+  
+const darkModeToggle = document.querySelector('input[name=tema]');
+darkModeToggle.addEventListener('change', function({ target }) {
+  setTema(target.checked ? 'dark' : 'light');
+});
+
+function setTema(novoTema) {
+    const CorTema = tema[novoTema]; // Seleciona o tema para aplicar
+  
+    Object.keys(CorTema).map(function(key) {
+      html.style.setProperty(`--${key}`, CorTema[key]); // Altera as variáveis no css
+    });
+  
+    localStorage.setItem('tema', novoTema); //Salva o tema escolhido no localStorage
+}
+  
+const tema = localStorage.getItem('tema');
+if( tema ) {
+  setTema(tema)
+}
